108bfdae6d40b4a140133cb493a621b2d3f4fe98
nikitamishagin <nikitamishagin@gmail.com>
2022-05-22 21:14:31 +0300

    Add upgraded companion image

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t nikitamishagin/nginxproxy:latest ./proxy/.
+docker build -t nikitamishagin/acme-companion:latest ./acme/.
 docker stack deploy --compose-file ./docker-compose.yml reverse-proxy
